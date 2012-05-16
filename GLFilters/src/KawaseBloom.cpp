78fca260046d5703cfa1655d7dc599dede5f622a
Gabor Papp <gabor.papp@gmail.com>
2012-05-16 16:43:03 +0200

    minor fix

--- GLFilters/src/KawaseBloom.cpp
+++ GLFilters/src/KawaseBloom.cpp
@@ -1,5 +1,5 @@
 /*
- Copyright (C) 2011 Gabor Papp
+ Copyright (C) 2012 Gabor Papp
 
  This program is free software; you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
